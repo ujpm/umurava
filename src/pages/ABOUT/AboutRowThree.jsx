b128ba1278a048b9493b9cb358d82332495c2baf
ujpm <uwizeyimanajp2@gmail.com>
2025-02-04 11:38:58 +0200

    feat: Add new pages and improve page organization
    Added new pages:
    - About page with multiple sections
    - Challenge Hackathons pages and variants
    - Challenge Details pages
    - Converted all pages from TypeScript to JSX
    - Improved page organization with better directory structure

---
+++ src/pages/ABOUT/AboutRowThree.jsx
@@ -0,0 +1,39 @@
+          <Button size="8xl" className="min-w-[8.13rem] rounded-[10px] px-[2.13rem] font-semibold sm:px-[1.25rem]">
+            Join
+          </Button>
+        </div>
+      </div>
+    </ModalProvider>
+  );
+}
+
+              </>
+            </Text>
+          </div>
+              <>
+                Get notified on the latest projects
+                <br />
+                and hackathons
+              Join our WhatsApp community
+            </Heading>
+            <Text size="text4xl" as="p" className="self-stretch text-center text-[1.13rem] font-normal leading-[145%]">
+              as="h1"
+              className="text-[1.50rem] font-semibold !text-[#101828] md:text-[1.38rem]"
+            >
+          </div>
+          <div className="flex w-[32%] flex-col items-center justify-center gap-[0.50rem] md:w-full">
+            <Heading
+              size="heading6xl"
+              alt="Boldduotone"
+              className="h-[3.88rem] w-full md:h-auto"
+            />
+          <div className="w-[12%] rounded-[66px] bg-[#d0e0fc] p-[2.13rem] md:w-full sm:p-[1.25rem]">
+            <Img
+              src="images/img_bold_duotone_messages.svg"
+  return (
+    <ModalProvider {...props} appElement={document.getElementById("root")} isOpen={isOpen} className="min-w-[90.00rem]">
+      <div className="w-full min-w-[90.00rem] rounded-[12px] border border-solid border-[#e4e7ec] bg-[#ffffff] py-[3.75rem] md:py-[1.25rem]">
+        <div className="container-xs flex flex-col items-center gap-[2.50rem] px-[3.50rem] md:px-[1.25rem]">
+import { default as ModalProvider } from "react-modal";
+
+export default function Community({ isOpen, ...props }) {
