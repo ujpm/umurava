607fc326d5643d3e50f05960928f2ec1bb71adbe
ujpm <uwizeyimanajp2@gmail.com>
2025-02-04 10:00:21 +0200

    Add project files and assets

---
+++ src/pages/Home1/Home1ColumnSix.jsx
@@ -0,0 +1,66 @@
+workforce of digital professionals.{" "}
+</Text>
+</div>
+<div className="flex flex-col items-center gap-[5.13rem] md:gap-[3.81rem] sm:gap-[2.56rem]">
+<div className="flex gap-[1.63rem] self-stretch md:flex-col">
+  <Suspense fallback={<div>Loading feed...</div>}>
+    {[...Array(3)].map((d, index) => (
+      <HomeChallengecard key={"listopen" + index} />
+    ))}
+  </Suspense>
+</div>
+<Button
+  size="9xl"
+  variant="outline"
+  shape="round"
+  className="min-w-[12.88rem] rounded-md !border px-[2.06rem] font-semibold tracking-[0.00rem] sm:px-[1.25rem]"
+>
+  View More
+</Button>
+</div>
+</div>
+</div>
+);
+}
+
+  className="self-stretch text-center !font-['DM_Sans'] text-[1.13rem] font-normal leading-[150%] !text-[#677488]"
+>
+  Join Skills Challenges Program to accelerate your career growth and become part of Africa’s largest
+            workforce of digital professionals.{" "}
+          </Text>
+        </div>
+        <div className="flex flex-col items-center gap-[5.13rem] md:gap-[3.81rem] sm:gap-[2.56rem]">
+          <div className="flex gap-[1.63rem] self-stretch md:flex-col">
+            <Suspense fallback={<div>Loading feed...</div>}>
+              {[...Array(3)].map((d, index) => (
+                <HomeChallengecard key={"listopen" + index} />
+              ))}
+            </Suspense>
+          </div>
+          <Button
+            size="9xl"
+            variant="outline"
+            shape="round"
+            className="min-w-[12.88rem] rounded-md !border px-[2.06rem] font-semibold tracking-[0.00rem] sm:px-[1.25rem]"
+          >
+            View More
+          </Button>
+        </div>
+      </div>
+            className="self-stretch text-center !font-['DM_Sans'] text-[1.13rem] font-normal leading-[150%] !text-[#677488]"
+          >
+            Join Skills Challenges Program to accelerate your career growth and become part of Africa’s largest
+          <Text
+            size="text4xl"
+            as="p"
+    </div>
+  );
+}
+
+          >
+            Explore Challenges & Hackathons{" "}
+          </Heading>
+          <Heading
+            size="heading10xl"
+            as="h2"
+            className="text-[2.50rem] font-bold tracking-[0.00rem] !text-[#03192e] md:text-[2.38rem] sm:text-[2.25rem]"
